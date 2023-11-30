e6aa2705f60e037b47950ff83bb5f56ffd5377be
markjosmild <markjosmild@gmail.com>
2023-11-30 10:51:21 +0800

    chore: middleware update

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,15 +1,19 @@
 const jwt = require('../utilities/jwt')
 
 module.exports = async function (ctx, next) {
-  const bearerHeader = ctx.request.headers.authorization
-  const token = bearerHeader.split(' ')[1]
-  const verifiedToken = jwt.verify(token)
+  try {
+    const bearerHeader = ctx.request.headers.authorization
+    const token = bearerHeader.split(' ')[1]
+    const verifiedToken = jwt.verify(token)
 
-  if (Date.now() >= verifiedToken.exp * 1000) {
+    if (Date.now() >= verifiedToken.exp * 1000) {
+      ctx.throw(401)
+    }
+
+    ctx.state.auth = verifiedToken
+
+    return next()
+  } catch (error) {
     ctx.throw(401)
   }
-
-  ctx.state.auth = verifiedToken
-
-  return next()
 }
